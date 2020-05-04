84d6235f86c6b956945aaa464d1d9aa04a5fe4ae
canozo <canwoahzo@gmail.com>
2020-05-03 22:37:48 -0600

    Quitar filepond

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import UploadForm from './components/UploadFile/UploadForm';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Login}/>
+    <Route path="/upload" component={UploadForm}/>
     <Route path="/about" component={() => <div>About</div>}/>
     <Route component={() => <div>Not Found</div>}/>
   </Switch>
--- src/index.jsx
+++ src/index.jsx
@@ -7,18 +7,6 @@ import store from './redux/store';
 import App from './App';
 import './index.scss';
 
-// TODO remove these:
-import UploadFile from './components/UploadFile/UploadFile';
-import { registerPlugin } from 'react-filepond';
-import FilePondPluginValidateTypes from 'filepond-plugin-file-validate-type';
-import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
-import 'filepond/dist/filepond.min.css';
-
-registerPlugin(
-  FilePondPluginValidateTypes,
-  FilePondPluginFileValidateSize,
-);
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
