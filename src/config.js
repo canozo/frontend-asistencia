9bef6be79c114f54cbdbcc11753a72dd77a6c8e3
canozo <canwoahzo@gmail.com>
2020-05-17 18:56:58 -0600

    Server

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const globals = {
   // TODO
-  server: 'http://localhost:5000',
-  // server: 'http://ec2-3-86-140-112.compute-1.amazonaws.com',
+  // server: 'http://localhost:5000',
+  server: 'http://ec2-3-86-140-112.compute-1.amazonaws.com',
   regex: {
     username: /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))/,
     password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/,
