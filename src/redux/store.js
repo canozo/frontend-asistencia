7ae1a074a1356a9db138984c4b57059b25c45510
canozo <canwoahzo@gmail.com>
2020-07-27 07:27:05 -0600

    Fix vulnerabilidades

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './modules';
 import { loadState, saveState } from './localState';
 
-const store = createStore(rootReducer, loadState(), applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  loadState(),
+  composeWithDevTools(applyMiddleware(thunk))
+  );
 
 store.subscribe(() => {
   saveState({ auth: store.getState().auth });
